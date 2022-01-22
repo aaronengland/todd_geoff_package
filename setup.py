c708aeabb648a63b301a6887f8e57aa03c0fde8b
aaronengland <aaron.england.dev@gmail.com>
2022-01-22 12:47:55 -0700

    created setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='todd_geoff_package',
+      version='0.1',
+      description="Learning Python",
+      url='https://github.com/aaronengland/todd_geoff_package',
+      author='Aaron England',
+      author_email='aaron.england24.com',
+      license='MIT',
+      packages=['todd_geoff_package'],
+      zip_safe=False)
